50eaf5cd0d7e133c5651d32e0b8783d0a7e88eb7
Marlon <marlon@marlonharder.nl>
2017-02-13 16:30:50 +0100

    added instructions

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ var app = document.getElementById('app');
 if (window.location.hostname.indexOf("editthispost.com") > -1) {
   var indexJsonSource = 'http://editthispost.com:9002/api/1/getText?apikey=b6fa274cc2b92cc4db05dfc787b0cb2372b4ce505ed6f852b93e677549e08760&padID=index';
 } else {
-  var indexJsonSource = './sampleIndex.json';
+  // var indexJsonSource = './sampleIndex.json';
+  var indexJsonSource = 'http://editthispost.com:9002/api/1/getText?apikey=b6fa274cc2b92cc4db05dfc787b0cb2372b4ce505ed6f852b93e677549e08760&padID=index';
+  
 }
 
 mainRoute.initRouter();
