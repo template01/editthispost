786cecc166bd4923f9bb0ed4971fcadda4ca2f1a
lasbian <lasbian@debian.home>
2018-01-16 16:47:37 +0100

    textual changes

--- js/mainRoute.js
+++ js/mainRoute.js
@@ -45,7 +45,7 @@ var mainRoute = function () {
                     mainIndex.initIndex();
 
                     if ($('#singlePad').length === 0) {
-                        // mainIndex.playSplash();
+                        mainIndex.playSplash();
                     }
 
                     ranIndex = true;
